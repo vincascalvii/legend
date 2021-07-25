9a997de3376ae831825ba56772a08dd4a2494f85
vincascalvii <vincascalvii95@gmail.com>
2021-07-26 00:26:42 +1000

    Added basic search bar function

--- js/all.js
+++ js/all.js
@@ -4,14 +4,19 @@
 
 ================================================================ */
 
+// Get the search bar and all the pokemon blocks
 var searchBar = document.getElementById('search-bar');
 var pokemons = document.querySelectorAll('.pokedex .block');
 
+// Run the check when user enters an input
 searchBar.onkeyup = function() {
+
+	// Get the search term and convert it to lowercase
 	var searchTerm = document.getElementById('search-bar').value;
 	if ( searchTerm ) searchTerm.toLowerCase();
-	console.log(searchTerm);
-	console.log(pokemons.innterText);
+
+	// Run a loop through all the pokemon blocks' content
+	// If it matches, display the blocks, otherwise, hide them
 	for ( var i = 0; i < pokemons.length; i++ ) {
 		pokemons[i].innerText.toLowerCase().indexOf(searchTerm) != -1
 		? pokemons[i].classList.add('active')
