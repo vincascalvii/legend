3e9aedf1b5900e5608d544d6cd60031841f24b8c
vincascalvii <vincascalvii95@gmail.com>
2021-07-28 00:36:44 +1000

    Test pokedex database

--- js/all.js
+++ js/all.js
@@ -1,5 +1,82 @@
 /* ================================================================
 
+	POKEDEX
+
+================================================================ */
+
+// Fetch the data
+fetch('/legend/data/all/all.json')
+.then( response => {
+	if (!response.ok) throw new Error("HTTP error " + response.status);
+    return response.json();
+})
+.then( data => {
+
+	console.log(data);
+
+	var container = document.querySelector('.pokedex .container');
+
+	for ( var i = 0; i < data.length; i++ ) {
+
+		var number = document.createElement('p');
+			number.classList.add('number');
+			number.innerHTML = data[i]['number'];
+
+		var name = document.createElement('p');
+			name.classList.add('name');
+			name.innerHTML = data[i]['name'];
+
+		var type = document.createElement('div');
+			type.classList.add('types');
+			type.innerHTML = data[i]['type'];
+
+		var info = document.createElement('div');
+			div.classList.add('info');
+			div.appendChild(number);
+			div.appendChild(name);
+			div.appendChild(type);
+
+		var image = document.createElement('picture');
+			image.innerHTML = '<source srcset="/legend/img/pokemon/' + data[i]['number'] 
+							+ '/' + data[i]['image_1'] + '.webp 1x, '
+							+ '/legend/img/pokemon/' + data[i]['number'] 
+							+ '/' + data[i]['image_2'] + '.webp 2x, '
+							+ '/legend/img/pokemon/' + data[i]['number'] 
+							+ '/' + data[i]['image_3'] + '.webp 3x" type="image/webp">'
+							+ '<img src="/legend/img/pokemon/' + data[i]['number'] 
+							+ '/' + data[i]['image_1'] + '.png 1x, '
+							+ '/legend/img/pokemon/' + data[i]['number'] 
+							+ '/' + data[i]['image_2'] + '.png 2x, '
+							+ '/legend/img/pokemon/' + data[i]['number'] 
+							+ '/' + data[i]['image_3'] + '.png 3x" class="image" alt="Leafen">';
+
+		var background = document.createElement('picture');
+			background.innerHTML = '<source srcset="/legend/img/others/pokedex-corner-200x200.webp" '
+								 + 'type="image/webp">'
+								 + '<img src="/legend/img/others/pokedex-corner-200x200.png" '
+							 	 + 'class="background" alt="Pokeball background">';
+
+		var block = document.createElement('a');
+			block.classList.add('block', 'active');
+			block.setAttribute('aria-label', data[i]['name']);
+			block.appendChild(info);
+			block.appendChild(image);
+			block.appendChild(background);
+
+		container.appendChild(block);
+
+	}
+
+})
+.catch( function(error) {
+	console.log('Fetch error: ', error);
+});
+
+
+
+
+/* ================================================================
+
 	SEARCH BAR
 
 ================================================================ */
